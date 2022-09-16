40348d63c7b45ca9ba3e5f93653f3604675e79b9
Santiago Moncada <90731161+SantiMoncada@users.noreply.github.com>
2022-09-16 10:21:58 +0200

    final tweaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { catService } from './services/test.service';
-import { NgbdModalComponent } from './popup-details/popup-details.component'
 
 @Component({
   selector: 'app-root',
